f4bdffd69dd4188de9b11d0e7266bc2916f6eecf
toan <thanhtoan0306@gmail.com>
2023-08-05 01:16:17 +0700

    ejs2

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-// Setting EJS as templating engine
-app.set("view engine", "ejs");
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
 app.set("views", "./views");
+app.get("/", (req, res) => {
+    res.render("index", { variableName: "Hello World 12212!" })
+})
 
-// Routing
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-// Starting the server
-app.listen(3000);
+app.listen(3000, (req, res) => {
+    console.log("App is running on port 3000")
+})

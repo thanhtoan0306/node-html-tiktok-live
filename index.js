0f8eaef94fd2be86854d7ceb2c88df17801fdada
toan <thanhtoan0306@gmail.com>
2023-08-05 00:12:53 +0700

    firstt init

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><h1>Hello World2!</h1></body></html>');
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});

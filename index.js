5965ffdef04db050ea4264fb42f7cf4513f21be0
toan <thanhtoan0306@gmail.com>
2023-08-05 00:16:51 +0700

    add start command

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><h1>Hello World2!</h1></body></html>');
+    res.write('<html><body><h1>Hello World3!</h1></body></html>');
     res.end();
 });
 

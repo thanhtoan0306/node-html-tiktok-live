e7327693cca993f9448bd4ad45e409103efca662
toan <thanhtoan0306@gmail.com>
2023-08-05 12:23:42 +0700

    plain html

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
-const ejs = require('ejs');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
 let counter = 0;
-
-app.set('view engine', 'ejs');
-
 app.get('/', (req, res) => {
-    res.render('index', { counter });
+    const indexHtmlPath = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(indexHtmlPath);
 });
 
 io.on('connection', (socket) => {
@@ -22,5 +20,5 @@ setInterval(() => {
 }, 1000);
 
 http.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+    console.log('Server started on http://localhost:3001');
 });

4a49644ad2beaaf0456d9852cb1c8df2e44a5c4a
Thanh Toan <thanhtoan0306@gmail.com>
2024-06-29 14:43:10 +0700

    first-template

---
+++ app/tiktoklive/model.js
@@ -0,0 +1,66 @@
+const uni= {
+    code: 'UTE',
+    avatar: '',
+    vote: 0
+}
+
+const SCHOOL_LIST = [
+    {
+      "name": "Khoa học Xã hội và Nhân văn",
+      "code": "USSH",
+      "avatar": ""
+    },
+    {
+      "name": "Luật TP.HCM",
+      "code": "UL",
+      "avatar": ""
+    },
+    {
+      "name": "Sư phạm Kỹ thuật TP.HCM",
+      "code": "UTE",
+      "avatar": ""
+    },
+    {
+      "name": "Bách khoa TP.HCM",
+      "code": "BKA",
+      "avatar": ""
+    },
+    {
+      "name": "Khoa học Tự nhiên",
+      "code": "US",
+      "avatar": ""
+    },
+    {
+      "name": "Công nghệ Thông tin",
+      "code": "UIT",
+      "avatar": ""
+    },
+    {
+      "name": "Kinh tế Luật TP.HCM",
+      "code": "UEL",
+      "avatar": ""
+    },
+    {
+      "name": "Quốc tế",
+      "code": "IU",
+      "avatar": ""
+    },
+    {
+      "name": "Sư phạm TP.HCM",
+      "code": "SPS",
+      "avatar": ""
+    },
+    {
+      "name": "Ngoại thương TP.HCM Cơ sở 2",
+      "code": "FTU2",
+      "avatar": ""
+    },
+    {
+      "name": "Y Dược TP.HCM",
+      "code": "YDS",
+      "avatar": ""
+    }
+  
+
+
+]

2049d607720aaf0d1487ca810f33663bcc68e086
Thanh Toan <thanhtoan0306@gmail.com>
2024-06-29 15:57:20 +0700

    UI tam dc

--- app/tiktoklive/model.js
+++ app/tiktoklive/model.js
@@ -1,66 +1,75 @@
-const uni= {
-    code: 'UTE',
-    avatar: '',
-    vote: 0
-}
+const uni = {
+  code: "UTE",
+  avatar: "",
+  vote: 0,
+};
 
 const SCHOOL_LIST = [
-    {
-      "name": "Khoa học Xã hội và Nhân văn",
-      "code": "USSH",
-      "avatar": ""
-    },
-    {
-      "name": "Luật TP.HCM",
-      "code": "UL",
-      "avatar": ""
-    },
-    {
-      "name": "Sư phạm Kỹ thuật TP.HCM",
-      "code": "UTE",
-      "avatar": ""
-    },
-    {
-      "name": "Bách khoa TP.HCM",
-      "code": "BKA",
-      "avatar": ""
-    },
-    {
-      "name": "Khoa học Tự nhiên",
-      "code": "US",
-      "avatar": ""
-    },
-    {
-      "name": "Công nghệ Thông tin",
-      "code": "UIT",
-      "avatar": ""
-    },
-    {
-      "name": "Kinh tế Luật TP.HCM",
-      "code": "UEL",
-      "avatar": ""
-    },
-    {
-      "name": "Quốc tế",
-      "code": "IU",
-      "avatar": ""
-    },
-    {
-      "name": "Sư phạm TP.HCM",
-      "code": "SPS",
-      "avatar": ""
-    },
-    {
-      "name": "Ngoại thương TP.HCM Cơ sở 2",
-      "code": "FTU2",
-      "avatar": ""
-    },
-    {
-      "name": "Y Dược TP.HCM",
-      "code": "YDS",
-      "avatar": ""
-    }
-  
+  {
+    name: "Sư phạm Kỹ thuật",
+    code: "UTE",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Khoa học Xã hội và Nhân văn",
+    code: "USSH",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Luật",
+    code: "UL",
+    avatar: "",
+    votes: 0,
+  },
 
-
-]
+  {
+    name: "Bách khoa",
+    code: "BKA",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Khoa học Tự nhiên",
+    code: "US",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Công nghệ Thông tin",
+    code: "UIT",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Kinh tế Luật",
+    code: "UEL",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Quốc tế",
+    code: "IU",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Sư phạm",
+    code: "SPS",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Ngoại thương Cơ sở 2",
+    code: "FTU2",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Y Dược",
+    code: "YDS",
+    avatar: "",
+    votes: 0,
+  },
+];

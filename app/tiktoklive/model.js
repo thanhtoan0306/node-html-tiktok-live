ba1ac6d68527ac926f0b40d6f3c7d3814c011673
Thanh Toan <thanhtoan0306@gmail.com>
2024-06-29 17:12:21 +0700

    tam oke

--- app/tiktoklive/model.js
+++ app/tiktoklive/model.js
@@ -73,3 +73,40 @@ const SCHOOL_LIST = [
     votes: 0,
   },
 ];
+
+// {
+//     "emotes": [],
+//     "comment": "T1 nhiều bố quá vậy :))))",
+//     "userId": "6641015491610968066",
+//     "secUid": "MS4wLjABAAAA3pHTY5ZNuWNQmkvPb8uTfkft06OaPpxsqFsyX8k_EKu8XVZ3FaHUNZLo1uSCZTop",
+//     "uniqueId": "69969696danh",
+//     "nickname": "🍍🍍🍍🍍🍍🍍",
+//     "profilePictureUrl": "https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/9c4be96460054d573e1bf871dc9ee2c6.webp?lk3s=a5d48078&nonce=58623&refresh_token=055a6a503d229fa6dda44c2466248d15&x-expires=1719824400&x-signature=%2FunmgmNuRR%2BGGPjqobqbaTw5T6M%3D&shp=a5d48078&shcp=fdd36af4",
+//     "followRole": 1,
+//     "userBadges": [],
+//     "userSceneTypes": [],
+//     "userDetails": {
+//         "createTime": "0",
+//         "bioDescription": "",
+//         "profilePictureUrls": [
+//             "https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/9c4be96460054d573e1bf871dc9ee2c6~tplv-tiktok-shrink:72:72.webp?lk3s=a5d48078&nonce=11660&refresh_token=a64345ffb01280ffe0546c28a8159ac8&x-expires=1719824400&x-signature=UbeRb5WwHhnA2H39oRiGUpQkUA8%3D&shp=a5d48078&shcp=fdd36af4",
+//             "https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/9c4be96460054d573e1bf871dc9ee2c6.webp?lk3s=a5d48078&nonce=58623&refresh_token=055a6a503d229fa6dda44c2466248d15&x-expires=1719824400&x-signature=%2FunmgmNuRR%2BGGPjqobqbaTw5T6M%3D&shp=a5d48078&shcp=fdd36af4",
+//             "https://p9-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/9c4be96460054d573e1bf871dc9ee2c6.webp?lk3s=a5d48078&nonce=31507&refresh_token=bf37e40eb8c67530316e9e436ba12fe4&x-expires=1719824400&x-signature=GtCmTub8hPtbrWAVsa1kCd5GjF8%3D&shp=a5d48078&shcp=fdd36af4",
+//             "https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/9c4be96460054d573e1bf871dc9ee2c6.jpeg?lk3s=a5d48078&nonce=19010&refresh_token=1746dba7fee648b4eb585e8f5b633aed&x-expires=1719824400&x-signature=zREP5HFNbVnVXWxRQcr49saEeaU%3D&shp=a5d48078&shcp=fdd36af4"
+//         ]
+//     },
+//     "followInfo": {
+//         "followingCount": 271,
+//         "followerCount": 2,
+//         "followStatus": 1,
+//         "pushStatus": 0
+//     },
+//     "isModerator": false,
+//     "isNewGifter": false,
+//     "isSubscriber": false,
+//     "topGifterRank": null,
+//     "gifterLevel": 0,
+//     "teamMemberLevel": 0,
+//     "msgId": "7385847666221566741",
+//     "createTime": "1719651676647"
+// }
--- public/index.js
+++ public/index.js
@@ -1,64 +1,249 @@
- const socket = io();
-      var liveNode = document.getElementById("live-view");
-      const modelPlots = [
-        {
-          name: "---",
-          avatar: "",
-          userId: 0,
-        },
-      ];
-      const maxPlotter = 15;
-      let plotter = [];
-      let index_replace = 0;
+const socket = io();
+const SCHOOL_LIST = [
+  {
+    name: "Sư phạm Kỹ thuật",
+    code: "UTE",
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b9/Logo_Tr%C6%B0%E1%BB%9Dng_%C4%90%E1%BA%A1i_H%E1%BB%8Dc_S%C6%B0_Ph%E1%BA%A1m_K%E1%BB%B9_Thu%E1%BA%ADt_TP_H%E1%BB%93_Ch%C3%AD_Minh.png",
+    votes: 0,
+  },
+  {
+    name: "Khoa học Xã hội và Nhân văn",
+    code: "USSH",
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/vi/4/49/Logo_Tr%C6%B0%E1%BB%9Dng_%C4%90%E1%BA%A1i_h%E1%BB%8Dc_Khoa_h%E1%BB%8Dc_X%C3%A3_h%E1%BB%99i_v%C3%A0_Nh%C3%A2n_v%C4%83n%2C_%C4%90%E1%BA%A1i_h%E1%BB%8Dc_Qu%E1%BB%91c_gia_Th%C3%A0nh_ph%E1%BB%91_H%E1%BB%93_Ch%C3%AD_Minh.svg",
+    votes: 0,
+  },
+  {
+    name: "Luật",
+    code: "UL",
+    avatar: "",
+    votes: 0,
+  },
 
-      socket.on("member", (data) => {
-        if (data) {
-          console.log("member-log", data);
-          const customer = {
-            avatar:
-              data?.userDetails?.profilePictureUrls[2] ||
-              data.profilePictureUrl ||
-              "",
-            name: data.nickname || "---",
-          };
-          let innerHTML = `
-          <div class="avatar-user card">
-            <div class="shadow"></div>
-            <img
-              src="${customer.avatar}"
-            />
-          </div>
-          <div class="name-user typewriter content">
-            <h1 class="name">${customer.name}</h1>
-          </div>`;
-          liveNode.innerHTML = innerHTML;
+  {
+    name: "Bách khoa",
+    code: "BKA",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOq8y1ujU7NbHbUitI9x8awQtGTYeibk1JLw&s",
+    votes: 0,
+  },
+  {
+    name: "Khoa học Tự nhiên",
+    code: "US",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Công nghệ Thông tin",
+    code: "UIT",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Kinh tế Luật",
+    code: "UEL",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Quốc tế",
+    code: "IU",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Sư phạm",
+    code: "SPS",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Ngoại thương Cơ sở 2",
+    code: "FTU",
+    avatar: "",
+    votes: 0,
+  },
+  {
+    name: "Y Dược",
+    code: "YDS",
+    avatar: "",
+    votes: 0,
+  },
+];
+let listRank = SCHOOL_LIST;
+let totalVotes = 1;
+// var liveNode = document.getElementById("live-view");
+// const modelPlots = [
+//   {
+//     name: "---",
+//     avatar: "",
+//     userId: 0,
+//   },
+// ];
+// const maxPlotter = 15;
+// let plotter = [];
+// let index_replace = 0;
+
+// socket.on("member", (data) => {
+//   if (data) {
+//     console.log("member-log", data);
+//     const customer = {
+//       avatar:
+//         data?.userDetails?.profilePictureUrls[2] ||
+//         data.profilePictureUrl ||
+//         "",
+//       name: data.nickname || "---",
+//     };
+//     let innerHTML = `
+//     <div class="avatar-user card">
+//       <div class="shadow"></div>
+//       <img
+//         src="${customer.avatar}"
+//       />
+//     </div>
+//     <div class="name-user typewriter content">
+//       <h1 class="name">${customer.name}</h1>
+//     </div>`;
+//     liveNode.innerHTML = innerHTML;
+//   }
+// });
+
+socket.on("chat", (data) => {
+  if (data) {
+    console.log("chat", data);
+    const newComment = data.comment;
+    console.log("newComment", newComment);
+    const matchingSchool = listRank.find(
+      (school) =>
+        String(school.code).toLocaleLowerCase() ===
+        String(data.comment).toLocaleLowerCase()
+    );
+    if (matchingSchool) {
+      // Increase votes by 1 for the matched school
+      matchingSchool.votes++;
+      listRank.sort((a, b) => b.votes - a.votes);
+      // Calculate total votes using reduce
+      totalVotes = listRank.reduce((accumulator, currentSchool) => {
+        return accumulator + currentSchool.votes;
+      }, 0);
+      console.log(listRank);
+      var top1Node = document.getElementById("top1");
+      let innerHTMLTop1 = `            
+      <div class="top1 unit-uni">
+              <div>
+                <div class="avatar-uni">
+                  <img
+                    width="auto"
+                    height="180px"
+                    class="img-uni"
+                    src="${listRank[0].avatar}"
+                  />
+                </div>
+              </div>
+              <div class="code-uni">${listRank[0].votes}</div>
+              <div style="height: ${
+                (listRank[0].votes / totalVotes) * 400
+              }px" class="column-rank">
+                <div class="e-card playing">
+                  <div class="wave"></div>
+                  <div class="wave"></div>
+                  <div class="wave"></div>
+                </div>
+              </div>
+              <div class="code-uni">Top 1 - ${listRank[0].code}</div>
+            </div>`;
+      top1Node.innerHTML = innerHTMLTop1;
+      var top2Node = document.getElementById("top2");
+      let innerHTMLTop2 = `
+                        <div class="top2 unit-uni">
+              <div>
+                <div class="avatar-uni">
+                  <img
+                    width="auto"
+                    height="180px"
+                    class="img-uni"
+                    src="${listRank[1].avatar}"
+                  />
+                </div>
+              </div>
+              <div class="code-uni">${listRank[1].votes}</div>
+              <div style="height: 400px" class="column-rank"></div>
+              <div class="code-uni">Top 2 - ${listRank[1].code}</div>
+            </div>     
+      `;
+      top2Node.innerHTML = innerHTMLTop2;
+      var top3Node = document.getElementById("top3");
+      let innerHTMLTop3 = `
+                        <div class="top3 unit-uni">
+              <div>
+                <div class="avatar-uni">
+                  <img
+                    width="auto"
+                    height="180px"
+                    class="img-uni"
+                    src="${listRank[2].avatar}"
+                  />
+                </div>
+              </div>
+              <div class="code-uni">${listRank[2].votes}</div>
+              <div style="height: 400px" class="column-rank"></div>
+              <div class="code-uni">Top 3 - ${listRank[2].code}</div>
+            </div>     
+      `;
+      top3Node.innerHTML = innerHTMLTop3;
+      var top4Node = document.getElementById("top4");
+      listRank.forEach((schoolItem, index) => {
+        if (index > 2 && index < 10) {
+          const schoolItemNode = document.createElement("div");
+          schoolItemNode.className = "user";
+          const innerHTMLSchool = `
+         <div class="image">
+                      <img
+                        src="${schoolItem.avatar}"
+                      />
+                    </div>
+                    <div class="user__content">
+                      <div class="text">
+                        <div>${schoolItem.code} : ${
+            schoolItem.votes
+          } votes</div>
+                      </div>
+                      <button class="follow">Top ${index + 1}</button>
+                    </div>
+        `;
+          schoolItemNode.innerHTML = innerHTMLSchool;
+          top4Node.appendChild(schoolItemNode);
         }
       });
+    }
+  }
+});
 
-      const submitData = () => {
-        // Retrieve the input value
-        var inputData = document.getElementById("myInput").value;
+const submitData = () => {
+  // Retrieve the input value
+  var inputData = document.getElementById("myInput").value;
 
-        fetch("/submit", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ inputData: inputData }),
-        })
-          .then((response) => response.json())
-          .then((data) => console.log(data))
-          .catch((error) => console.log(error));
-      };
+  fetch("/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ inputData: inputData }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
 
-      const disconnectTitktok = () => {
-        fetch("/stop", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => console.log(data))
-          .catch((error) => console.log(error));
-      };
+const disconnectTitktok = () => {
+  fetch("/stop", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
